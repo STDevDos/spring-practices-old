a5e746762262fbc2f3760e09a6a41f0f6f2aa53b
Froy <fdaf58@gmail.com>
2019-11-14 02:12:03 -0600

    boom

--- springboot-soap/src/main/java/com/techprimers/spring_boot_soap_example/GetUserResponse.java
+++ springboot-soap/src/main/java/com/techprimers/spring_boot_soap_example/GetUserResponse.java
@@ -2,20 +2,24 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.11.13 a las 10:36:56 PM CST 
+// Generado el: 2019.11.14 a las 01:46:43 AM CST 
 //
 
 
 package com.techprimers.spring_boot_soap_example;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 
 /**
  * <p>Clase Java para anonymous complex type.
- *
+ * 
  * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
- *
+ * 
  * <pre>
  * &lt;complexType>
  *   &lt;complexContent>
@@ -27,10 +31,12 @@ import javax.xml.bind.annotation.*;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
+ * 
+ * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-        "user"
+    "user"
 })
 @XmlRootElement(name = "getUserResponse")
 public class GetUserResponse {
@@ -40,9 +46,11 @@ public class GetUserResponse {
 
     /**
      * Obtiene el valor de la propiedad user.
-     *
-     * @return possible object is
-     * {@link User }
+     * 
+     * @return
+     *     possible object is
+     *     {@link User }
+     *     
      */
     public User getUser() {
         return user;
@@ -50,9 +58,11 @@ public class GetUserResponse {
 
     /**
      * Define el valor de la propiedad user.
-     *
-     * @param value allowed object is
-     *              {@link User }
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link User }
+     *     
      */
     public void setUser(User value) {
         this.user = value;
--- springboot-soap/src/main/java/com/techprimers/spring_boot_soap_example/ObjectFactory.java
+++ springboot-soap/src/main/java/com/techprimers/spring_boot_soap_example/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.11.13 a las 10:36:56 PM CST 
+// Generado el: 2019.11.14 a las 01:46:43 AM CST 
 //
 
 
@@ -12,17 +12,18 @@ import javax.xml.bind.annotation.XmlRegistry;
 
 
 /**
- * This object contains factory methods for each
- * Java content interface and Java element interface
- * generated in the com.techprimers.spring_boot_soap_example package.
- * <p>An ObjectFactory allows you to programatically
- * construct new instances of the Java representation
- * for XML content. The Java representation of XML
- * content can consist of schema derived interfaces
- * and classes representing the binding of schema
- * type definitions, element declarations and model
- * groups.  Factory methods for each of these are
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.techprimers.spring_boot_soap_example package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
  * provided in this class.
+ * 
  */
 @XmlRegistry
 public class ObjectFactory {
@@ -30,12 +31,14 @@ public class ObjectFactory {
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.techprimers.spring_boot_soap_example
+     * 
      */
     public ObjectFactory() {
     }
 
     /**
      * Create an instance of {@link GetUserResponse }
+     * 
      */
     public GetUserResponse createGetUserResponse() {
         return new GetUserResponse();
@@ -43,6 +46,7 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link User }
+     * 
      */
     public User createUser() {
         return new User();
@@ -50,6 +54,7 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link GetUserRequest }
+     * 
      */
     public GetUserRequest createGetUserRequest() {
         return new GetUserRequest();
