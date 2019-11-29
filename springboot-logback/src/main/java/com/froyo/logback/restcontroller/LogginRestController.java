b2bfa0b48c147aa14b867bd6c51475aa3b7584fb
Froy <fdaf58@gmail.com>
2019-11-29 02:45:11 -0600

    logback

--- springboot-logback/src/main/java/com/froyo/logback/restcontroller/LogginRestController.java
+++ springboot-logback/src/main/java/com/froyo/logback/restcontroller/LogginRestController.java
@@ -1,7 +1,9 @@
 package com.froyo.logback.restcontroller;
 
+import eu.bitwalker.useragentutils.UserAgent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,25 @@ public class LogginRestController {
     private final Logger logger = LoggerFactory.getLogger(LogginRestController.class);
 
     @RequestMapping() // default Mapping
-    public String index() {
+    public String index(@RequestHeader("User-Agent") String userAgent) {
+
+        UserAgent userAgentObject = UserAgent.parseUserAgentString(userAgent);
+
+        //
 
         logger.trace("A TRACE Message");
         logger.debug("A DEBUG Message");
+
+        //By default
         logger.info("An INFO Message");
         logger.warn("A WARN Message");
         logger.error("An ERROR Message");
+
+        logger.trace("doStuff needed more information - {}", userAgentObject);
+        logger.debug("doStuff needed to debug - {}", userAgentObject);
+        logger.info("doStuff took input - {}", userAgentObject.getId());
+        logger.warn("doStuff needed to warn - {}", userAgentObject.getBrowserVersion());
+        logger.error("doStuff encountered an error with value - {}", userAgentObject.getOperatingSystem());
 
         return "SUCCESS : default mapping";
     }
