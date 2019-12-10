0dc818ae088cf88ad5eab1e4dfcdabd05544b9dd
Froy <fdaf58@gmail.com>
2019-12-10 00:49:53 -0600

    example @Async

---
+++ springboot-async-employeeservice/src/main/java/com/froyo/SpringBootAsyncEmployeeService.java
@@ -0,0 +1,12 @@
+package com.froyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsyncEmployeeService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAsyncEmployeeService.class, args);
+	}
+}
---
+++ springboot-async-service/src/main/java/com/froyo/controller/AsyncController.java
@@ -0,0 +1,40 @@
+package com.froyo.controller;
+
+import com.froyo.model.EmployeeAddresses;
+import com.froyo.model.EmployeeNames;
+import com.froyo.service.AsyncService;
+import com.froyo.model.EmployeePhone;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@Slf4j
+@RestController
+public class AsyncController {
+
+	@Autowired
+	private AsyncService service;
+
+	@RequestMapping(value = "/testAsynch", method = RequestMethod.GET)
+	public void testAsynch() throws InterruptedException, ExecutionException 
+	{
+		log.info("testAsynch Start");
+
+		CompletableFuture<EmployeeAddresses> employeeAddress = service.getEmployeeAddress();
+		CompletableFuture<EmployeeNames> employeeName = service.getEmployeeName();
+		CompletableFuture<EmployeePhone> employeePhone = service.getEmployeePhone();
+
+		// Wait until they are all done
+		CompletableFuture.allOf(employeeAddress, employeeName, employeePhone).join();
+		
+		log.info("EmployeeAddress--> " + employeeAddress.get());
+		log.info("EmployeeName--> " + employeeName.get());
+		log.info("EmployeePhone--> " + employeePhone.get());
+	}
+}
