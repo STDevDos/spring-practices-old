8d2d5e459ccfc1c4691b9df18c8481412b64f3cd
Froy <fdaf58@gmail.com>
2020-01-07 13:19:26 -0600

    enviando todo el contenido antes de que se me acabe la licencia ;)

---
+++ springboot-flowable/src/main/java/com/froyo/flowable/AppFlowable.java
@@ -0,0 +1,13 @@
+package com.froyo.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFlowable {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFlowable.class,args);
+    }
+
+}
